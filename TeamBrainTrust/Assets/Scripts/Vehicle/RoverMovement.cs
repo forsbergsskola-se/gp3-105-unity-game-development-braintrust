edca8f6824ce13bde5197ef428875e6f6bad22aa
augustlewen <augustlewen@live.com>
2023-10-17 14:20:54 +0200

    Added drive forward and reverse to the Roover movement

--- TeamBrainTrust/Assets/Scripts/Vehicle/RoverMovement.cs
+++ TeamBrainTrust/Assets/Scripts/Vehicle/RoverMovement.cs
@@ -6,15 +6,35 @@ namespace Vehicle
     public class RoverMovement : MonoBehaviour
     {
         public int durability;
-        public float speed;
+        
+        public float maxSpeed;
+        public float maxReverseSpeed;
         public float acceleration;
-
+        
+        private float currentSpeed;
+        
         private void Update()
         {
-            if (Input.GetButton("Vertical"))
+            //Press forward button to begin accelerating
+            if (Input.GetAxis("Vertical") > 0)
             {
-                
+                if (currentSpeed <= maxSpeed)
+                {
+                    currentSpeed += acceleration * Time.deltaTime;
+                }
             }
+
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                if (currentSpeed > maxReverseSpeed)
+                {
+                    currentSpeed -= acceleration * Time.deltaTime;
+                }
+            }
+
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, currentSpeed);
+
         }
     }
 }
