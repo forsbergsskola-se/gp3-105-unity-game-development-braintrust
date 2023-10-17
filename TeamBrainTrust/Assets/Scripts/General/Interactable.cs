71fc207597ca471fb072877fd87e350021dfa853
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-17 16:39:43 +0200

    Interactable script added

---
+++ TeamBrainTrust/Assets/Scripts/General/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace General
+{
+    public class Interactable : MonoBehaviour
+    {
+        private bool isPlayerInReach;
+        public UnityEvent onInteraction;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            isPlayerInReach = true;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            isPlayerInReach = false;
+        }
+    }
+}
