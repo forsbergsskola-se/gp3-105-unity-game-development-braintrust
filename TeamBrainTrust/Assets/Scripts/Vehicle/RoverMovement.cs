df5b26bc3b30badfa78ccd5e178766b70bafe883
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-17 14:00:12 +0200

    Added RoverMovment

---
+++ TeamBrainTrust/Assets/Scripts/Vehicle/RoverMovement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Vehicle
+{
+    public class RoverMovement : MonoBehaviour
+    {
+        public int durability;
+        public float speed;
+        public float acceleration;
+
+        private void Update()
+        {
+            if (Input.GetButton("Vertical"))
+            {
+                
+            }
+        }
+    }
+}
