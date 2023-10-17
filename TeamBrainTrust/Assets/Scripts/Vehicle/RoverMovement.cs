83ee94601dcebdb20cc4726d36a0577a127c39d1
augustlewen <augustlewen@live.com>
2023-10-17 14:37:56 +0200

    Added lerp to smoothly break the rover when there is no input

--- TeamBrainTrust/Assets/Scripts/Vehicle/RoverMovement.cs
+++ TeamBrainTrust/Assets/Scripts/Vehicle/RoverMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Vehicle
 {
@@ -10,31 +11,43 @@ namespace Vehicle
         public float maxSpeed;
         public float maxReverseSpeed;
         public float acceleration;
+        public float breakPower;
         
         private float currentSpeed;
         
         private void Update()
         {
             //Press forward button to begin accelerating
-            if (Input.GetAxis("Vertical") > 0)
+            if (Input.GetButton("Vertical"))
             {
-                if (currentSpeed <= maxSpeed)
+                if (Input.GetAxis("Vertical") > 0)
                 {
-                    currentSpeed += acceleration * Time.deltaTime;
+                    if (currentSpeed <= maxSpeed)
+                    {
+                        currentSpeed += acceleration * Time.deltaTime;
+                    }
+                }
+
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if (currentSpeed > maxReverseSpeed)
+                    {
+                        currentSpeed -= acceleration * Time.deltaTime;
+                    }
                 }
             }
-
-            if (Input.GetAxis("Vertical") < 0)
+            else
             {
-                if (currentSpeed > maxReverseSpeed)
+                if (currentSpeed != 0)
                 {
-                    currentSpeed -= acceleration * Time.deltaTime;
+                    currentSpeed = Mathf.Lerp(currentSpeed, 0, breakPower);
+                    
                 }
             }
 
 
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, currentSpeed);
-
+            
         }
     }
 }
