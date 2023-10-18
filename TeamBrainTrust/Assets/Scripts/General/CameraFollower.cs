85226d273eee9312e7d09a876ccf7bc6e076e20e
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-10-18 13:31:27 +0200

    Add camera follower script

--- TeamBrainTrust/Assets/Scripts/General/CameraFollower.cs
+++ TeamBrainTrust/Assets/Scripts/General/CameraFollower.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public GameObject player;
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (player.activeSelf)
+        {
+            Vector3 target = player.transform.position + new Vector3(0, 0, -7.5f);
+            transform.position = target;
+        }
     }
+    
 }
