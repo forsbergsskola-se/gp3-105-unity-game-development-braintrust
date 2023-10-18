84ea26c51e0bd8e7c87b181e555a9ac38694e786
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-18 10:41:45 +0200

    PlayerStats created added take dmg and deathmethod

---
+++ TeamBrainTrust/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int maxHealth;
+        private int currentHealth;
+
+        private void Start()
+        {
+            currentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+            if(currentHealth <= 0)
+            {
+                Death();
+            }
+        }
+
+        private void Death()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
