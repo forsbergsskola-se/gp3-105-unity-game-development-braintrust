ca57eda6e6c5a7734ab1d07669749415324e623e
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-19 14:03:16 +0200

    Namespace changes

--- TeamBrainTrust/Assets/Scripts/General/CameraFollower.cs
+++ TeamBrainTrust/Assets/Scripts/General/CameraFollower.cs
@@ -1,27 +1,33 @@
+using System;
 using Player;
 using UnityEngine;
 
-public class CameraFollower : MonoBehaviour
+namespace General
 {
-    private GameObject target;
-    public float maxDistance = 0.5f;
-    private float targetSpeed;
-    
-    private void LateUpdate()
+    public class CameraFollower : MonoBehaviour
     {
-        FollowTarget();
-    }
+        private GameObject target;
+        public float maxDistance = 0.5f;
+        private float targetSpeed;
 
-    public void SetTarget(GameObject target, float targetSpeed)
-    {
-        this.target = target;
-        this.targetSpeed = targetSpeed;
+        private void LateUpdate()
+        {
+            FollowTarget();
+        }
+
+        public void SetTarget(GameObject target, float targetSpeed)
+        {
+            this.target = target;
+            this.targetSpeed = targetSpeed;
+        }
+
+        private void FollowTarget()
+        {
+            Vector3 targetPosition = target.transform.position + new Vector3(0, 0, -7.5f);
+            //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * targetSpeed / maxDistance);
+            transform.position = Vector3.Lerp(transform.position, targetPosition,
+                Time.deltaTime * (targetSpeed / maxDistance));
+        }
+
     }
-    
-    private void FollowTarget()
-    {
-        Vector3 targetPosition = target.transform.position + new Vector3(0, 0, -7.5f);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * targetSpeed / maxDistance);
-    }
-    
 }
