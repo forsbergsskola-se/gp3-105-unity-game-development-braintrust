1231fdd9199b770408c6ac20a81e77938b10f91c
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-23 09:46:16 +0200

    Added Pickup Method

---
+++ TeamBrainTrust/Assets/Scripts/Items/PickUpItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using General;
+using UnityEngine;
+
+namespace Items
+{
+    public class PickUpItem : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<Interactable>().onInteraction.AddListener(Pickup);
+        }
+
+        private void Pickup(GameObject player)
+        {
+            transform.parent = player.transform;
+        }
+    }
+}
