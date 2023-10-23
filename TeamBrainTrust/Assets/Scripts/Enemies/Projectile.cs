7429278cd5eaf1f75c2ad15147b7ca72615c607f
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-23 15:32:26 +0200

    Added projectile to gamescene

--- TeamBrainTrust/Assets/Scripts/Enemies/Projectile.cs
+++ TeamBrainTrust/Assets/Scripts/Enemies/Projectile.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Enemies
 {
     public class Projectile : MonoBehaviour
     {
         public float speed;
-        
+        public int damage;
+
+        private void Update()
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+        }
+
+        public void SetupProjectile(Vector3 rotation)
+        {
+            transform.Rotate(rotation);
+            
+        }
     }
 }
