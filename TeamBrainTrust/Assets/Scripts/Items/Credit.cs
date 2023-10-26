ea847bbe0166708c4811fa5074c2d218ceff46ff
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-26 12:04:29 +0200

    Rename Coin to Credit

---
+++ TeamBrainTrust/Assets/Scripts/Items/Credit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TMPro;
+using UI;
+using UnityEngine;
+
+namespace Items
+{
+    public class Credit : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                PlayerHUD.i.UpdateCredits();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
