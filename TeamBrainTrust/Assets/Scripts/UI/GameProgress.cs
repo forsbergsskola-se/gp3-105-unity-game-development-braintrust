5c51ec8897b7e990fe04111064c5497792b506f2
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-26 10:17:15 +0200

    GameProgress Script update

--- TeamBrainTrust/Assets/Scripts/UI/GameProgress.cs
+++ TeamBrainTrust/Assets/Scripts/UI/GameProgress.cs
@@ -1,11 +1,27 @@
-﻿using UnityEngine;
-
+﻿using TMPro;
+using UnityEngine;
+using Quest;
 namespace UI
 {
     public class GameProgress : MonoBehaviour
     {
-        
+        public TextMeshProUGUI progressText;
+        public int progressValue;
+        private void Start()
+        {
+            GetComponent<QuestManager>().OnQuestCompleted.AddListener(UpdateShipUI);
+            
+        }
+        public void UpdateShipUI() // Updates text in UI
+        {
+            progressValue = 25;
+            progressText.text = $"{progressValue}% COMPLETED";
+        }
     }
+    
+    
+    
+    
     
     /// public TextMeshProUGUI scoreText;
     /// public int scoreCount;
