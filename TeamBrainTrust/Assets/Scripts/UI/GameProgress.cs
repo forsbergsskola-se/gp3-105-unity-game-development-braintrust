98f032498003da85c2951556b20653c6fb86bc5b
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-26 09:46:57 +0200

    GameProgress created, Score script moved

---
+++ TeamBrainTrust/Assets/Scripts/UI/GameProgress.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class GameProgress : MonoBehaviour
+    {
+        
+    }
+    
+    /// public TextMeshProUGUI scoreText;
+    /// public int scoreCount;
+    
+    /* public void UpdateScore()
+    {
+
+    scoreCount = 25;
+    scoreText.text = $"{scoreCount}% COMPLETED";
+            
+            
+
+    }*/
+}
