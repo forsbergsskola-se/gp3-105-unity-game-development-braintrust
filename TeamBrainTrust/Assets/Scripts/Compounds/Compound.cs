5bbc6f00d6c4e017e316beb6b564daee9da9807e
augustlewen <augustlewen@live.com>
2023-10-26 13:53:16 +0200

    Added parking space for compoounds

--- TeamBrainTrust/Assets/Scripts/Compounds/Compound.cs
+++ TeamBrainTrust/Assets/Scripts/Compounds/Compound.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Items;
 using Quest;
 using UnityEngine;
 
@@ -7,6 +8,24 @@ namespace Compounds
     public class Compound : MonoBehaviour
     {
         public int crates;
+        public Transform cratesTransform;
+        public ParkingSpace parkingSpace;
+        private bool isObjective;
+
+        public PickUpItem[] items;
+        
+        public void SetIsObjective(bool isObjective)
+        {
+            this.isObjective = isObjective;
+            parkingSpace.gameObject.SetActive(isObjective);
+
+            foreach (Transform child in cratesTransform)
+            {
+                child.gameObject.SetActive(isObjective);
+            }
+
+        }
+        
 
         private void OnTriggerEnter2D(Collider2D other)
         {
---
+++ TeamBrainTrust/Assets/Scripts/Compounds/ParkingSpace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Quest;
+using UnityEngine;
+
+namespace Compounds
+{
+    public class ParkingSpace : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.layer == LayerMask.NameToLayer("Rover"))
+            QuestManager.i.OnEnterCompound();
+        }
+    }
+}
