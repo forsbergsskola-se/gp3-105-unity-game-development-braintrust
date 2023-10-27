d28f88957d59e07c8d55cbdb2c2c91601c7ee652
augustlewen <augustlewen@live.com>
2023-10-27 12:28:56 +0200

    Added consumable and vendin machine

---
+++ TeamBrainTrust/Assets/Scripts/General/VendingMachine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using General;
+
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
+
+namespace General
+{
+    public class VendingMachine : MonoBehaviour
+    {
+        public GameObject consumablePrefab;
+        public int cost;
+        
+        private Vector2 spitForceInterval = new Vector2(85, 150);
+        
+        private void Start()
+        {
+            GetComponent<Interactable>().onInteraction.AddListener(OnInteraction);
+        }
+
+        private void OnInteraction(GameObject player)
+        {
+            //Check if player credits >= cost
+            SpitConsumable();
+        }
+
+
+        private void SpitConsumable()
+        {
+            float angle = Random.Range(0, 360) * Mathf.Deg2Rad;
+            float x = Mathf.Cos(angle);
+            float y = Mathf.Sin(angle);
+            
+            float spitForce = Random.Range(spitForceInterval.x, spitForceInterval.y);
+            GameObject credit = Instantiate(consumablePrefab, transform.position, quaternion.identity);
+            credit.GetComponent<Rigidbody2D>().AddForce(new Vector2(x,y) * spitForce);
+        }
+    }
+}
---
+++ TeamBrainTrust/Assets/Scripts/Items/Consumable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace Items
+{
+    public class Consumable : MonoBehaviour
+    {
+        public int healPower;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                other.gameObject.GetComponent<PlayerStats>().Heal(healPower);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
