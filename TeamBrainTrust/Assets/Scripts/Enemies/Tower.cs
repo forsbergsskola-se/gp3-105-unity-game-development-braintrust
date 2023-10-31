7db34f6c1be3aeacbd41b68edeea7fb6ffafd9c7
augustlewen <augustlewen@live.com>
2023-10-31 10:52:36 +0100

    Updated tower art and made tower rotate towards player

--- TeamBrainTrust/Assets/Scripts/Enemies/Tower.cs
+++ TeamBrainTrust/Assets/Scripts/Enemies/Tower.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Player;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -7,9 +8,29 @@ namespace Enemies
     public class Tower : MonoBehaviour
     {
         public GameObject projectilePrefab;
-        public float range;
         public float reloadTime;
         private float countdown;
+        private bool isPlayerInRange = false;
+        public Transform canonTransform;
+        private GameObject target;
+        public float rotationDamping;
+
+        private void Start()
+        {
+            SetTargetToPlayer();
+            PlayerStats.i.OnEnterRover.AddListener(SetTargetToRover);
+            PlayerStats.i.OnExitRover.AddListener(SetTargetToPlayer);
+
+        }
+
+        private void SetTargetToPlayer()
+        {
+            target = PlayerStats.i.gameObject;
+        }
+        private void SetTargetToRover()
+        {
+            target = PlayerStats.i.rover.gameObject;
+        }
 
         private void Update()
         {
@@ -17,6 +38,8 @@ namespace Enemies
             {
                 return;
             }
+
+            RotateTowardsTarget();
             
             countdown -= Time.deltaTime;
             
@@ -28,11 +51,19 @@ namespace Enemies
             }
         }
 
+        private void RotateTowardsTarget()
+        {
+            Vector3 targetLookPos = Quaternion.Euler(0, 0, 180) * (target.transform.position - transform.position);
+            Quaternion rotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: targetLookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping); 
+        }
+
         private bool IsTowerActive()
         {
-            //Check if Player is in range?
-            
-            //Check if player is on current Objective?
+            if (!isPlayerInRange)
+            {
+                return false;
+            }
     
             return true;
         }
@@ -42,6 +73,23 @@ namespace Enemies
             Instantiate(projectilePrefab, transform.position, transform.rotation);
         }
 
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player") || other.gameObject.layer == LayerMask.NameToLayer("Rover"))
+            {
+                isPlayerInRange = true;
+                target = other.gameObject;
+            }
+        }
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player") || other.gameObject.layer == LayerMask.NameToLayer("Rover"))
+            {
+                isPlayerInRange = false;
+            }
+        }
+        
     }
     
 }
