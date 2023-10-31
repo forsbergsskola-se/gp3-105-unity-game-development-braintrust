b0b4d33d767f66f37fc282b7509b7bc3dc778a00
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2023-10-31 11:48:21 +0100

    Updated Main menu script

---
+++ TeamBrainTrust/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public string targetSceneName;
+        
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(targetSceneName);
+        }
+        
+        public void EndGame()
+        {
+            Application.Quit();
+        }
+
+        public void OpenGameRules()
+        {
+            
+        }
+    }
+    
+    
+}
