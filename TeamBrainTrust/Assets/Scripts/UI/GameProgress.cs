9a364e254bdd8cd8b09feb5ac9b96a5cb37a35d8
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-11-01 15:01:00 +0100

    Small adjustments on gamescene

--- TeamBrainTrust/Assets/Scripts/UI/GameProgress.cs
+++ TeamBrainTrust/Assets/Scripts/UI/GameProgress.cs
@@ -11,12 +11,12 @@ namespace UI
         private void Start()
         {
             QuestManager.i.OnQuestCompleted.AddListener(UpdateShipUI);
-
         }
 
         public void UpdateShipUI() // Updates text in UI
         {
-            progressValue = 25;
+            Debug.Log(progressValue);
+            progressValue += 25;
             GetComponent<TextMeshProUGUI>().text = $"{progressValue}% COMPLETED";
         }
     }
