f952ad7d576706014f92e96fda5ee5d8eaf132fe
augustlewen <augustlewen@live.com>
2023-11-02 08:12:21 +0100

    updated boxes so when picked up it looks like it's in the players hands when its rotating around

--- TeamBrainTrust/Assets/Scripts/Items/PickUpItem.cs
+++ TeamBrainTrust/Assets/Scripts/Items/PickUpItem.cs
@@ -8,15 +8,51 @@ namespace Items
 {
     public class PickUpItem : MonoBehaviour
     {
+
+        private bool isPickedUp;
         private void Start()
         {
             GetComponent<Interactable>().onInteraction.AddListener(Pickup);
+        }
+
+        private void Update()
+        {
+            if(!isPickedUp)
+                return;
+
+            int xInput = (int)Input.GetAxisRaw("Horizontal");
+            int yInput = (int)Input.GetAxisRaw("Vertical");
+
+            if (yInput > 0 && !Input.GetButton("Horizontal"))
+            {
+                GetComponent<SpriteRenderer>().sortingOrder = 8;
+                transform.localPosition = new Vector3(0, 0.3f, 0);
+            }
+            else if (yInput < 0 && !Input.GetButton("Horizontal"))
+            {
+                GetComponent<SpriteRenderer>().sortingOrder = 11;
+                transform.localPosition = new Vector3(0, -0.35f, 0);
+            }
+            
+            if (xInput > 0 && !Input.GetButton("Vertical"))
+            {
+                GetComponent<SpriteRenderer>().sortingOrder = 8;
+                transform.localPosition = new Vector3(0.40f, 0, 0);
+            }
+            else if (xInput < 0 && !Input.GetButton("Vertical"))
+            {
+                GetComponent<SpriteRenderer>().sortingOrder = 8;
+                transform.localPosition = new Vector3(-0.40f, 0, 0);
+            }
+            
+            
         }
 
         private void Pickup(GameObject player)
         {
             if(player.GetComponent<PlayerStats>().itemInHand != null)
                 return;
+            isPickedUp = true;
             
             SoundManager.PlaySound("Pick Up Crate");
             transform.parent = player.transform;
