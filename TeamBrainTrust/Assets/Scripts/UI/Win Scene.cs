2bb8e88f20f9b0a53c8f07e4de59bd0492ca3ec7
augustlewen <augustlewen@live.com>
2023-11-02 11:16:55 +0100

    Added quit button to win scene

---
+++ TeamBrainTrust/Assets/Scripts/UI/Win Scene.cs
@@ -0,0 +1,20 @@
+﻿using Systems.General;
+using UnityEngine;
+
+namespace UI
+{
+    public class Win_Scene : MonoBehaviour
+    {
+        private void Start()
+        {
+            //Play Win Music
+            //SoundManager.PlaySound("Main Menu Music");
+        }
+        
+        public void EndGame()
+        {
+            Application.Quit();
+        }
+        
+    }
+}
